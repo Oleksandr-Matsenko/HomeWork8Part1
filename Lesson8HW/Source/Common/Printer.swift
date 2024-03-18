e9ba57d7cff139a2143d46963afac08218218141
Oleksandr-Matsenko <olexandr282@gmail.com>
2024-03-18 22:12:51 +0200

    Update Printer.swift
    Complete 8.1 task

--- Lesson8HW/Source/Common/Printer.swift
+++ Lesson8HW/Source/Common/Printer.swift
@@ -9,9 +9,9 @@ import Foundation
 
 class Printer {
     
-    var yellowViewController: YellowViewController?
-    var blueViewController: BlueViewController?
-    var redViewController: RedViewController?
+    weak var yellowViewController: YellowViewController?
+    weak var blueViewController: BlueViewController?
+    weak var redViewController: RedViewController?
     
     private var timer: Timer?
     private var seconds: Int = 0
